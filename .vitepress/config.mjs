d69cbb6d8a817dd98e0a9ebc2623270fa20a7e95
Y1teng <to.hitakiti@gmail.com~>
2026-05-30 22:28:25 +0900

    first

---
+++ .vitepress/config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vitepress'
+import { withMermaid } from 'vitepress-plugin-mermaid'
+
+// https://vitepress.dev/reference/site-config
+export default withMermaid(defineConfig({
+  title: "42docs",
+  description: "Summarize the issues",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+}))
---
+++ libft.md
@@ -0,0 +1,9 @@
+# libft
+First page!
+
+## List of Function
+```mermaid
+graph TD
+    A[ft_strlen] --> B[文字列の長さを返す]
+    A --> C[引数: char *s]
+```
